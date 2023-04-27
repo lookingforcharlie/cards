5c8bd42cc6cda2695b77b58a521fb1b642ba8510
lookingforcharlie <charliefeng2020@gmail.com>
2023-04-27 17:33:34 -0400

    1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
